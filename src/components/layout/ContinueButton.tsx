1156d318ad912f746c170d9a72c7e7c4d09b0640
codeBerzerk <alphatech664@gmail.com>
2024-04-30 14:56:12 +0300

    feat: adaptived

--- src/components/layout/ContinueButton.tsx
+++ src/components/layout/ContinueButton.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import styles from '../../styles/continue.module.scss';
+import styles from '@/styles/continue.module.scss';
 
 interface ContinueButtonProps {
     onContinue: () => void;
     isContinueEnabled: boolean;
 }
 
-const ContinueButton: React.FC<ContinueButtonProps> = ({ onContinue, isContinueEnabled }) => {
+const ContinueButton: React.FC<ContinueButtonProps> = ({onContinue, isContinueEnabled}) => {
     return (
-        <footer className={styles.footer}>
-            <button className={styles.continueButton} disabled={!isContinueEnabled} onClick={onContinue}>
-                Continue
-            </button>
-        </footer>
+        <button className={styles.continueButton} disabled={!isContinueEnabled} onClick={onContinue}>
+            Continue
+        </button>
     );
 };
 
